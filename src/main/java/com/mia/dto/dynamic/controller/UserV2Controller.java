982665f2be035c80aa8eb33d193a4c05aa4719fa
halitkalayci <halit.kalayci@ext.etiya.com>
2024-07-04 11:20:25 +0300

    latest

--- src/main/java/com/mia/dto/dynamic/controller/UserV2Controller.java
+++ src/main/java/com/mia/dto/dynamic/controller/UserV2Controller.java
@@ -13,20 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value = "/api/user", headers = "API-VERSION=1")
+@RequestMapping(value = "/api/user", headers = "API-VERSION=2")
 @RequiredArgsConstructor
-public class UserController {
+public class UserV2Controller {
     private final RoleBasedFieldService roleBasedFieldService;
 
 
+
     @GetMapping(value="")
-    public Map<String, Object> getDetails(@RequestParam Long roleId)
-    {
-        UserDetailsDto dto = new UserDetailsDto(1, "Halit","Kalaycı", "12345", new AddressDto(new CityDto("Ankara","06"),"abc"));
-
-        return roleBasedFieldService.filterFields(dto, roleId);
+    public String get2(@RequestParam String x) {
+        return x;
     }
-
 }
 
 // /api/v1/users
